9c87801a6a785cf932c38b85408c7e12db84ed5e
Pukar <pukarupreti3@gmai.com>
2021-05-16 12:06:38 +0545

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const colors = require('colors')
-const connectDB = require('./config/db')
+const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
  
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 dotenv.config()
 
@@ -19,8 +21,10 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
