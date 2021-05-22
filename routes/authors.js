d12f5b0a59b2d1ec202338b61efa98d6a9257573
Pukar Upreti <pukarupreti3@gmail.com>
2021-05-22 09:08:43 +0545

    Author Routes CRUD compeleted

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
+const author = require('../models/author');
 const router = express.Router();
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 
 //All Authors Route
@@ -40,6 +42,66 @@ router.post('/', async (req, res) => {
             author: author,
             errorMessage: 'Error Creating Author'
         })
+    }
+})
+
+// Show Author
+router.get('/:id', async (req,res) => {
+    try {
+    const author = await Author.findById(req.params.id) 
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+        author: author,
+        booksByAuthor: books
+    })
+    } catch  {  
+        res.redirect('/')
+    }
+})
+
+//Edit Author
+router.get('/:id/edit', async (req,res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', {author: author })
+    } catch (error) {
+        res.redirect('/authors')
+    }
+})
+
+//Update Author
+router.put('/:id', async(req,res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch (error) {
+        if( author == null){
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error Updating author'
+            })
+        }
+    }
+})
+
+//Delete Author
+router.delete('/:id', async(req,res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors')
+    } catch (error) {
+        if( author == null){
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
     }
 })
 
